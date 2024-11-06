4d531b717f3e32df1b525ef0372e59a52a391d59
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-11-05 18:08:54 -0800

    Add files via upload

---
+++ alterstatements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n=1;
+	while(n<10)
+	{
+		if(n ==5 )
+		{
+			n=n+1;
+			continue;
+		}
+		cout<<"n "<<n<<endl;
+		n = n+1;
+		
+	}
+}
